7122dc92af25b651226feb8f0a30421bb2942431
DatuX <edwin@datux.nl>
2023-11-02 23:01:36 +0100

    Update CliBase.py

--- zfs_autobackup/CliBase.py
+++ zfs_autobackup/CliBase.py
@@ -10,7 +10,7 @@ class CliBase(object):
     Overridden in subclasses that add stuff for the specific programs."""
 
     # also used by setup.py
-    VERSION = "3.3-beta"
+    VERSION = "3.3-beta.2"
     HEADER = "{} v{} - (c)2022 E.H.Eefting (edwin@datux.nl)".format(os.path.basename(sys.argv[0]), VERSION)
 
     def __init__(self, argv, print_arguments=True):
